6931dee445779acfa94e39db20cd1baa7e1c14bd
jb <jb751996@hotmail.fr>
2018-12-04 21:49:48 -0500

    fuzzy

--- Assets/Scripts/fuzzy/fuzzyTest.cs
+++ Assets/Scripts/fuzzy/fuzzyTest.cs
@@ -2,21 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fuzzyTest : MonoBehaviour {
 
-    public AnimationCurve test1;
-    public AnimationCurve test2;
-    public AnimationCurve test3;
+public class fuzzyTest:MonoBehaviour  {
+
+    public courbeTrapeze notSafe;
+    public courbeTriangle safe;
+    public courbeTrapeze tooSafe;
+
+    public courbeTrapeze closeJ;
+    public courbeTriangle mediumJ;
+    public courbeTrapeze farJ;
+
+    public courbeTrapeze closeI;
+    public courbeTriangle mediumI;
+    public courbeTrapeze farI;
+
+    public courbeTriangle slowVA;
+    public courbeTriangle mediumVA;
+    public courbeTrapeze fastVA;
 
     private void Start() {
-        float eval = 0.3f;
-        Debug.Log(test1.Evaluate(eval));
-        Debug.Log(test2.Evaluate(eval));
-        Debug.Log(test3.Evaluate(eval));
+        slowVA = new courbeTriangle(0, 0.2f, 0.4f);
+        mediumVA = new courbeTriangle(0.3f, 0.5f, 0.7f);
+        fastVA = new courbeTrapeze(0.6f, 0.8f, 1f,10f);
 
-        Keyframe[] keys=test1.keys;
-        for (int i = 0; i < keys.Length; i++) {
-            Debug.Log("t: " + keys[i].time + " y: " + keys[i].value+"out tangent"+ keys[i].outTangent);
+        closeJ = new courbeTrapeze(0,5,5f,15f);
+        mediumJ = new courbeTriangle(5f, 15f, 30f);
+        farJ = new courbeTrapeze(15f, 30f, 9000, 10000);
+
+        closeI = new courbeTrapeze(0, 5, 2.5f, 7.5f);
+        mediumI = new courbeTriangle(2.5f, 7.5f, 15f);
+        farI = new courbeTrapeze(7.5f, 15f, 9000, 10000);
+
+        notSafe = new courbeTrapeze(0, 5, 25f, 50f);
+        safe = new courbeTriangle(25f, 50f, 75f);
+        tooSafe = new courbeTrapeze(50f, 75f, 95f, 100f);
+
+
+        float res=fuzzyCalculate(15f,50f,0.1f);
+        Debug.Log("logique floue " + res);
+    }
+
+    public float fuzzyCalculate(float distanceJ,float distanceI,float vitesseNorm) {
+        float closeJ_var = closeJ.Evaluate(distanceJ);
+        float mediumJ_var = mediumJ.Evaluate(distanceJ);
+        float farJ_var = farJ.Evaluate(distanceJ);
+
+        float closeI_var = closeI.Evaluate(distanceI);
+        float mediumI_var = mediumI.Evaluate(distanceI);
+        float farI_var = farI.Evaluate(distanceI);
+
+        float slowVA_var = slowVA.Evaluate(vitesseNorm);
+        float mediumVA_var = mediumVA.Evaluate(vitesseNorm);
+        float fastVA_var = fastVA.Evaluate(vitesseNorm);
+
+        float maxNotSafe_var=0;
+        float maxSafe_var=0;
+        float maxTooSafe_var=0;
+
+        //fuzzy rules
+        if (closeJ_var > maxNotSafe_var)
+            maxNotSafe_var = closeJ_var;
+        if (farJ_var > maxTooSafe_var)
+            maxTooSafe_var = farJ_var;
+        if (Mathf.Min(mediumJ_var, closeI_var) > maxNotSafe_var)
+            maxNotSafe_var = Mathf.Min(mediumJ_var, closeI_var);
+        if (Mathf.Min(mediumJ_var, fastVA_var) > maxNotSafe_var)
+            maxNotSafe_var = Mathf.Min(mediumJ_var, fastVA_var);
+        if (Mathf.Min(mediumJ_var, Mathf.Max(mediumI_var,farI_var)) > maxSafe_var)
+            maxSafe_var = Mathf.Min(mediumJ_var, Mathf.Max(mediumI_var, farI_var));
+        if (Mathf.Min(mediumJ_var, Mathf.Max(slowVA_var, mediumVA_var)) > maxSafe_var)
+            maxSafe_var = Mathf.Min(mediumJ_var, Mathf.Max(slowVA_var, mediumVA_var));
+
+        //defuzzifier
+        float max = 0;
+        int indiceMax = 0;
+        float[] tab = { maxNotSafe_var, maxSafe_var , maxTooSafe_var };
+        for (int i = 0; i < 3; i++) {
+            if (tab[i] > max) {
+                max = tab[i];
+                indiceMax = i;
+            }
         }
-}
+        Debug.Log("max " + maxNotSafe_var+ "    "+ maxSafe_var + "     "+ maxTooSafe_var);
+        Debug.Log("max "+max);
+        Vector2 final;
+        switch (indiceMax) {
+            case 0:
+                final=notSafe.Coupe(max);
+                break;
+            case 1:
+                final=safe.Coupe(max);
+                break;
+            default:
+                final=tooSafe.Coupe(max);
+                break;
+        }
+        return (final.y+final.x)/2f;
+    }
 }
